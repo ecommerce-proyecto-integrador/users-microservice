87fcb88af11fe601a7bfc1c01ff19a00b26e367b
Diego Gonzalez <diego.gonzalez07@alumnos.ucn.cl>
2023-12-18 12:25:06 -0300

    Change BD conection

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,17 +22,27 @@ import { JwtModule } from '@nestjs/jwt'; // Asegúrate de importar JwtModule
           },
         },
       },
-    ]),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'mysql', // sin docker es localhost
-      port: 3306, // sin docker es 3308
-      username: 'user_crud',
-      password: 'root',
-      database: 'db_crud',
+    ]),TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'silly.db.elephantsql.com',
+      port: 5432,
+      username: 'elxuemtd',
+      password: 'LGo6aUDv0jkn-3bFRV8G9Kni9VA5CKDn',
+      database: 'elxuemtd',
       autoLoadEntities: true,
       synchronize: true,
     }),
+      /*
+    TypeOrmModule.forRoot({
+      type: 'mysql', //mysql
+      host: 'mysql', // mysql
+      port: 3306, // sin docker es 3308 // docker 3306
+      username: 'user_crud', // user_crud
+      password: 'root', // root
+      database: 'db_crud', // db_crud
+      autoLoadEntities: true,
+      synchronize: true,
+    }),*/
     TypeOrmModule.forFeature([Users,Recovery]),
     JwtModule.register({
       secret: 'tu_clave_secreta', // Reemplaza con tu clave secreta real
